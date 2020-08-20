b8febb017ace8db25fbc43bf27c5654ce8240adc
宋志永 <songzhiyong@shuidi-inc.com>
2020-08-20 14:09:36 +0800

    优化节流指令逻辑

--- src/directives/throttle.js
+++ src/directives/throttle.js
@@ -6,27 +6,35 @@
 export default {
   inserted: (el, binding) => {
     let timer = null
+    let timeOut = 500  // 默认节流时间
     let isThrottle = true // 外界影响节流因素
-    if( typeof binding.arg !== "number" ){
+    const pTimeOut = +binding.arg
+    if (Number.isNaN(pTimeOut)) {
       throw new Error('v-throttle: not a number')
+    } else {
+      timeOut = pTimeOut
     }
-    if( binding.value !== undefined ){
-      if( typeof binding.value !== "boolean" ){
+    if (binding.value !== undefined) {
+      if (typeof binding.value !== 'boolean') {
         throw new Error('v-throttle="" not a boolean')
       } else {
         isThrottle = binding.value
       }
     }
-    let timeOut = binding.arg || 500  //  节流时间
-    el.parentElement.addEventListener('click', (e) => {
-      if (timer || !isThrottle) {
-        e.stopPropagation()
-      } else {
-        timer = setTimeout(() => {
-          clearTimeout(timer)
-          timer = null
-        }, timeOut)
-      }
-    }, true)
-  }
+    if(pTimeOut === 0) return
+    el.parentElement.addEventListener(
+      'click',
+      (e) => {
+        if (timer || !isThrottle) {
+          e.stopPropagation()
+        } else {
+          timer = setTimeout(() => {
+            clearTimeout(timer)
+            timer = null
+          }, timeOut)
+        }
+      },
+      true
+    )
+  },
 }
