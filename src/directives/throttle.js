3fcc165ec840f1837692d89f61ee54ce6ad26e62
yu5200101 <33239431+yu5200101@users.noreply.github.com>
2020-08-24 19:18:35 +0800

    improve:优化数据类型提示 (#4)
    * improve:优化数据类型提示
    *  优化判断传参秒非负数
    
    Co-authored-by: yuruyuan <yuruyuan@shuidihuzhu.com>

--- src/directives/throttle.js
+++ src/directives/throttle.js
@@ -3,36 +3,47 @@
  * @date 2020/08/19
  * @description click节流，默认500ms
  */
+// 获取数据类型
+const getInstance = function (data) {
+  // "[object Array]"
+  const instanceStr = Object.prototype.toString.call(data)
+  const index = instanceStr.indexOf(' ')
+  return instanceStr.substring(index + 1, instanceStr.length - 1)
+}
 export default {
   inserted: (el, binding) => {
     let timer = null
-    let timeOut = 500  // 默认节流时间
     let isThrottle = true // 外界影响节流因素
-    const pTimeOut = +binding.arg
-    if (Number.isNaN(pTimeOut)) {
-      throw new Error('v-throttle: not a number')
-    } else {
-      timeOut = pTimeOut
+    const timeout = +binding.arg
+    // 判断过期时间
+    if (Number.isNaN(timeout)) {
+      console.error(`error in [v-throttle:arg], invalid for "arg", Expected Numeric, got ${getInstance(binding.arg)}.`)
+      return
     }
+    if (timeout < 0) {
+      console.error('error in [v-throttle:arg], invalid for "arg", Expected Nonnegative number')
+      return
+    }
+    // 判断外界影响节流因素
     if (binding.value !== undefined) {
       if (typeof binding.value !== 'boolean') {
-        throw new Error('v-throttle="" not a boolean')
-      } else {
-        isThrottle = binding.value
+        console.error(`error in [v-throttle="value"], invalid for "value", Expected Boolean, got ${getInstance(binding.value)}.`)
+        return
       }
+      isThrottle = binding.value
     }
-    if(pTimeOut === 0) return
-    el.parentElement.addEventListener(
+    el.parentElement && el.parentElement.addEventListener(
       'click',
       (e) => {
         if (timer || !isThrottle) {
           e.stopPropagation()
-        } else {
-          timer = setTimeout(() => {
-            clearTimeout(timer)
-            timer = null
-          }, timeOut)
+          return
         }
+        timer = setTimeout(() => {
+          clearTimeout(timer)
+          timer = null
+          // 未给值默认是500ms
+        }, timeout * 1000 || 500)
       },
       true
     )
