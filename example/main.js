71f711b07fe0fae91a2677d2593bbaea8c52ca4c
cyy <cuiyuanyuan@shuidihuzhu.com>
2020-08-28 16:35:49 +0800

    修改文件结构，github workflows

--- example/main.js
+++ example/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import throttle from '@'
-// import throttle from '../npm/vue-directive-throttle'
+// import throttle from '../dist/sd-throttle.min'
 
 Vue.use(throttle)
 Vue.config.productionTip = false
--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ export default {
     // 打包名称
     name: 'sd-throttle',
     // 打包的文件
-    file: 'npm/sd-throttle.js',
+    file: 'dist/sd-throttle.min.js',
     // 打包的格式，umd 支持 commonjs/amd/life 三种方式
     format: 'umd',
     // 启用代码映射，便于调试之用
